cfd985cab7c61547475dc954cda57ce2554d1e35
moriongit <132233572+moriongit@users.noreply.github.com>
2023-10-10 00:34:23 +0400

    Add files via upload

---
+++ Digit count/3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input your number here");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (number>0)
+{
+    number=number/10;
+    sum++;
+}
+Console.WriteLine(sum);
