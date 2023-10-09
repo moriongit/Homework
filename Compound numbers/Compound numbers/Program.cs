bfbb6f227f8d565dca32ccc12fdf29fec1767522
moriongit <132233572+moriongit@users.noreply.github.com>
2023-10-10 00:33:11 +0400

    Add files via upload

---
+++ Compound numbers/Compound numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+int checker = 1;
+for (int division = 2; division < number; division++)
+{
+    if ((number % division) == 0)
+    {
+        checker = 0;
+    }
+}
+
+if (checker == 1)
+    Console.WriteLine("Number is prime number.", number);
+else
+    Console.WriteLine("Number is not a prime number.", number);
