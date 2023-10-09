5e379c24b677d8a88972f0150d275ca9929eac72
moriongit <132233572+moriongit@users.noreply.github.com>
2023-10-10 00:30:33 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= number)
+{
+    if (i != number)
+        i = i * 2;
+    if (i == number)
+    {
+        Console.WriteLine("The number is power of 2");
+            break;
+    };
+}
+if (i > number)
+    Console.Write("no");
